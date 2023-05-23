3d1f17de95cbc9fb40253af89f9b5044997545f2
정태영 <jty419@naver.com>
2023-05-23 13:05:29 +0900

    Delete: FavoritePicture.java & Remove @Builder interest file

--- src/main/java/com/back/miru/model/dto/Interest.java
+++ src/main/java/com/back/miru/model/dto/Interest.java
@@ -4,10 +4,7 @@ package com.back.miru.model.dto;
 import lombok.*;
 
 @Getter
-@Builder
 @ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Interest {
 	private int no;
 	private String dongCode;
