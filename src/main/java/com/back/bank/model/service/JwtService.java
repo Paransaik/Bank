fed273f223d60b8ea50f81210ee5564a29f2b5c0
정태영 <jty419@naver.com>
2023-06-08 17:08:42 +0900

    Refactor: Value Jwt Key & Expiration Use application.yml

--- src/main/java/com/back/bank/model/service/JwtService.java
+++ src/main/java/com/back/bank/model/service/JwtService.java
@@ -1,24 +1,32 @@
-package com.back.bank.interceptor;
+package com.back.bank.model.service;
 
 import com.back.bank.model.dto.TokenDTO;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
-
     public static final Logger logger = LoggerFactory.getLogger(JwtService.class);
-    private static final String SALT = "dimple";
-    private long accessTokenValidTime = 60 * 60 * 1000;
-    private long refreshTokenValidTime = 30 * 60 * 60 * 1000;
+
+    @Value("${jwt.token.secret}")
+    private String secret;
+
+    @Value("${jwt.token.expiration}")
+    private long expiration;
+
+    private byte[] key;
+
+    @PostConstruct
+    public void init() {
+        this.key = secret.getBytes(StandardCharsets.UTF_8);
+    }
 
     public TokenDTO createAccessAndRefresh(String userEmail) {
         Claims claims = Jwts.claims().setSubject(userEmail);
@@ -30,18 +38,18 @@ public class JwtService {
                 .setHeaderParam("regDate", System.currentTimeMillis())
                 .setClaims(claims) // 정보 저장
                 .setIssuedAt(now) // 토큰 발행 시간
-                .setExpiration(new Date(now.getTime() + accessTokenValidTime)) // 만료 시간
-                .signWith(SignatureAlgorithm.HS256, SALT.getBytes(StandardCharsets.UTF_8))
+                .setExpiration(new Date(now.getTime() + expiration)) // 만료 시간
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
 
         String refreshToken = Jwts
                 .builder()
                 .setHeaderParam("typ", "JWT")
                 .setHeaderParam("regDate", System.currentTimeMillis())
-                .setClaims(claims) // 정보 저장
-                .setIssuedAt(now) // 토큰 발행 시간
-                .setExpiration(new Date(now.getTime() + refreshTokenValidTime)) // 만료 시간
-                .signWith(SignatureAlgorithm.HS256, SALT.getBytes(StandardCharsets.UTF_8))
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + 30L * expiration)) // 만료 시간
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
 
         return TokenDTO.builder()
@@ -51,16 +59,42 @@ public class JwtService {
                 .build();
     }
 
-    public String recreateAccessToken(String userEmail){
+    public String recreateAccessToken(String userEmail) {
         Claims claims = Jwts.claims().setSubject(userEmail);
         Date now = new Date();
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + accessTokenValidTime))
-                .signWith(SignatureAlgorithm.HS256, SALT.getBytes(StandardCharsets.UTF_8))
+                .setExpiration(new Date(now.getTime() + expiration))
+                .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
+    }
+
+    public boolean isExpired(String token) {
+        try {
+            final Date expiration = getClaimsFromToken(token).getExpiration();
+            return expiration.before(new Date());
+        } catch (ExpiredJwtException ex) {
+            return true;
+        }
+    }
+
+    /*public String getTypeFromToken(String token) {
+        return getClaimsFromToken(token).get("type", String.class);
+    }
+
+
+    public Long getUserSeqFromToken(String token) {
+        return getClaimsFromToken(token).get("userSeq", Long.class);
+    }*/
+
+    public Claims getClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     //	전달 받은 토큰이 제대로 생성된것인지 확인 하고 문제가 있다면 UnauthorizedException을 발생.
@@ -68,7 +102,7 @@ public class JwtService {
         try {
             Jws<Claims> token = Jwts
                     .parser()
-                    .setSigningKey(SALT.getBytes(StandardCharsets.UTF_8))
+                    .setSigningKey(key)
                     .parseClaimsJws(jwt);
             logger.debug("token {}", token);
             return true;
