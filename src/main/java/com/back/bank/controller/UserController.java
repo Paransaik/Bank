9fad68b6f2f6e0f4383141463cf3ac52905963a9
정태영 <jty419@naver.com>
2023-06-08 17:35:02 +0900

    Add: @author & @param & @return

--- src/main/java/com/back/bank/controller/UserController.java
+++ src/main/java/com/back/bank/controller/UserController.java
@@ -2,7 +2,7 @@ package com.back.bank.controller;
 
 import com.back.bank.model.dto.TokenDTO;
 import com.back.bank.model.dto.UserDTO;
-import com.back.bank.interceptor.JwtService;
+import com.back.bank.model.service.JwtService;
 import com.back.bank.model.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -37,26 +37,37 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 유저 등록
+     * @author  tyJeong
+     * @param   map: UserDTO
+     * @return  TokenDTO
+     *
+     * TODO: 2023-06-08 사용자 로그인 시 이메일 중복 검사
+     */
     @PostMapping
-    public ResponseEntity<String> registerUser(@RequestBody Map<String, String> map) throws Exception {
+    public ResponseEntity<TokenDTO> registerUser(@RequestBody Map<String, String> map) throws Exception {
         logger.debug("Map:: {}", map.toString());
         userService.registerUser(map);
         UserDTO loginUserDTO = userService.loginUser(map.get("id"), map.get("password"));
-        String token = "";
-        if (loginUserDTO != null) {
-            token = jwtService.createAccessAndRefresh("id", loginUserDTO.getId(), "token");
-            return new ResponseEntity<>(token, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(token, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        TokenDTO token;
+        if (loginUserDTO == null) return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        token = jwtService.createAccessAndRefresh(loginUserDTO.getEmail());
+        return new ResponseEntity<>(token, HttpStatus.OK);
     }
 
+    /**
+     * 유저 등록
+     * @author  tyJeong
+     * @param   map: UserDTO
+     * @return  tokenDTO: access token, refresh token
+     */
     @ApiOperation(value = "회원정보수정", response = Map.class)
     @PutMapping("/{id}")
     public ResponseEntity<Map<String, Object>> updateUser(
-            @RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) @PathVariable String id,
+            @RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true)
+            @PathVariable String id,
             @RequestBody Map<String, String> map) {
-        System.out.println("update User 호출");
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status;
         try {
@@ -66,17 +77,23 @@ public class UserController {
             resultMap.put("message", SUCCESS);
             status = HttpStatus.ACCEPTED;
         } catch (Exception e) {
-            logger.error("수정 실패 : {0}", e);
             resultMap.put("message", e.getMessage());
             status = HttpStatus.INTERNAL_SERVER_ERROR;
         }
         return new ResponseEntity<>(resultMap, status);
     }
 
+    /**
+     * 유저 등록
+     * @author  tyJeong
+     * @param   id: user id
+     * @return  message
+     */
     @ApiOperation(value = "회원정보삭제", response = Map.class)
     @DeleteMapping("/{id}")
     public ResponseEntity<Map<String, Object>> deleteUser(
-            @RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) @PathVariable String id) {
+            @RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true)
+            @PathVariable String id) {
         System.out.println("delete user 호출");
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status;
@@ -96,10 +113,17 @@ public class UserController {
         return new ResponseEntity<>(resultMap, status);
     }
 
+    /**
+     * 유저 등록
+     * @author  tyJeong
+     * @param   userDTO: user id, user password
+     * @return  tokenDTO: access token, refresh token
+     */
     @ApiOperation(value = "로그인", notes = "Access-token과 로그인 결과 메세지를 반환한다.", response = Map.class)
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> loginUser(
-            @RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) UserDTO userDTO) throws Exception {
+            @RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true)
+            UserDTO userDTO) throws Exception {
         Map<String, Object> resultMap = new HashMap<>();
 
         // 이메일 체크 한번 해야 됨
@@ -117,18 +141,33 @@ public class UserController {
         return new ResponseEntity<>(resultMap, status);
     }
 
+    /**
+     * 유저 등록
+     * @author  tyJeong
+     * @param   id: user id, email: user email
+     * @return  cnt
+     */
     @GetMapping("/{id}")
-    public ResponseEntity<Integer> checkPasswordFind(@PathVariable String id, @RequestParam String email) throws Exception {
+    public ResponseEntity<Integer> checkPasswordFind(
+            @PathVariable String id,
+            @RequestParam String email) throws Exception {
         int cnt;
         System.out.println("checkPasswordFind 실행");
         cnt = userService.checkPasswordFind(id, email);
         return new ResponseEntity<>(cnt, HttpStatus.OK);
     }
 
+    /**
+     * 유저 등록
+     * @author  tyJeong
+     * @param   id: user id, request
+     * @return  userDTO: user id, user password
+     */
     @ApiOperation(value = "회원인증", notes = "회원 정보를 담은 Token을 반환한다.", response = Map.class)
     @GetMapping("/info/{id}")
     public ResponseEntity<Map<String, Object>> getUserInfo(
-            @PathVariable("id") @ApiParam(value = "인증할 회원의 아이디.", required = true) String id,
+            @PathVariable("id") @ApiParam(value = "인증할 회원의 아이디.", required = true)
+            String id,
             HttpServletRequest request) {
         Map<String, Object> resultMap = new HashMap<>();
         HttpStatus status;
